fff74d7f596852c10320c47dcfdeccac6be6c092
Samantha Vu <svu008@bolt.cs.ucr.edu>
2019-02-04 11:36:43 -0800

    Final Checkpoint 2

--- grading/phong_shader.cpp
+++ grading/phong_shader.cpp
@@ -10,29 +10,29 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 {
     vec3 color;
     vec3 intensity_ambient = color_ambient * world.ambient_color * world.ambient_intensity;
-    vec3 intensity_diffuse;
-    vec3 intensity_specular;
+    vec3 intensity_diffuse, intensity_specular;
 
-    vec3 l;
+    vec3 shadow_ray, ray_vec, reflected_ray;
+    Ray shadow_ray_ray;
     Light *cur_light;
+    Hit intersection{};
 
-    for(unsigned int i = 0; i < (world.lights).size(); ++i) {
+    for(unsigned int i = 0; i < world.lights.size(); i++) {
         cur_light = world.lights.at(i);
 	
-        //shadow_ray = cur_light->position - intersection_point;
-        //shadow_ray_vec = cur_light->position - intersection_point; 
-	//intersection = world.Closest_Intersection(shadow_ray);
+        shadow_ray = cur_light->position - intersection_point; 
+	shadow_ray_ray.endpoint = intersection_point;
+        shadow_ray_ray.direction = (cur_light->position - intersection_point).normalized();
+        ray_vec = intersection_point - ray.endpoint;
+        reflected_ray = shadow_ray - 2 * dot(shadow_ray, normal) * normal;
+  
+        intersection = world.Closest_Intersection(shadow_ray_ray);
 
-        //if(intersection.object || intersection.dist <= shadow_ray_vec.magnitude() ) { //equal
-            intensity_diffuse += color_diffuse * cur_light->Emitted_Light(intersection_point - cur_light->position) * std::max(dot(normal.normalized(),-(intersection_point - (cur_light->position)).normalized()),0.0);
+        if(intersection.object || intersection.dist < shadow_ray.magnitude() ) { //equal
+            intensity_diffuse += color_diffuse * cur_light->Emitted_Light(shadow_ray) * std::max(dot(normal,shadow_ray.normalized()),0.0);
             
-            //view = ray.endpoint - intersection_point;
-            //ray_vec = intersection_point - cur_light->position; 
-            //reflection = ray_vec.normalized() - 2 * dot(ray_vec.normalized(),normal) * normal;
-            l = cur_light->position - intersection_point;
-
-            intensity_specular += color_specular * cur_light->Emitted_Light(intersection_point - cur_light->position) * pow(std::max(dot((intersection_point - ray.endpoint).normalized(),(l - 2 * dot(l,normal)*normal).normalized()),0.0),specular_power);
-        //}
+            intensity_specular += color_specular * cur_light->Emitted_Light(shadow_ray) * pow(std::max(dot(ray_vec.normalized(),reflected_ray.normalized()),0.0), specular_power);
+        }
     }
     
     color = intensity_ambient + intensity_diffuse + intensity_specular;
--- sphere.cpp
+++ sphere.cpp
@@ -8,25 +8,26 @@ Hit Sphere::Intersection(const Ray& ray, int part) const
     double b = 2 * dot(ray.direction, ray.endpoint - center);
     double c = dot(ray.endpoint - center, ray.endpoint - center) - radius * radius;
     double determinant = b * b - 4 * a * c;
+    double t = 0;
 
     if(determinant == 0) {
-        double t_positive = (-1 * b) / (2 * a);
-        if(t_positive >= small_t) { return {this, t_positive, part}; }
+        t = (-1 * b) / (2 * a);
     }
     else if(determinant > 0) {
         double t_positive = ((-1 * b) + sqrt(determinant)) / (2 * a);
         double t_negative = ((-1 * b) - sqrt(determinant)) / (2 * a);
-        if(t_positive >= small_t && t_positive < t_negative) { return {this, t_positive, part}; }
-	else if(t_negative >= small_t && t_negative < t_positive) { return {this, t_negative, part}; }
+        if(t_positive < t_negative) { t = t_positive; }
+	else { t = t_negative; }
     }
-    
-    return {nullptr, 0, part};
+
+    if(t >= small_t) { return {this, t, part}; }
+    else { return {0, 0, 0}; }
 }
 
 vec3 Sphere::Normal(const vec3& point, int part) const
 {
     vec3 normal;
-    normal = ((point - center)/radius); // compute the normal direction
+    normal = (point - center).normalized(); // compute the normal direction
     return normal;
 }
 
