35d8c057d4e448cd16a9442d17b9b0605c0cd9f6
Sam <svu008@ucr.edu>
2019-02-09 15:36:01 -0800

    Debug Test 23

--- reflective_shader.cpp
+++ reflective_shader.cpp
@@ -15,7 +15,7 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 	color = (1 - reflectivity) * shader->Shade_Surface(ray,intersection_point,normal,recursion_depth);
         vec3 view_ray = ray.endpoint - intersection_point;
         Ray reflected_ray(intersection_point, (2 * dot(view_ray,normal) * normal - view_ray).normalized());
-	color += /*(1 - reflectivity) * color*/ + reflectivity * world.Cast_Ray(reflected_ray,recursion_depth+1);
+	color += /*(1 - reflectivity) * color + */reflectivity * world.Cast_Ray(reflected_ray,recursion_depth+1);
     }    
     return color;
 }
