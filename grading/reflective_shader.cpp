c53ea42ecbc68e8331f4218a67c96d68ee49b682
Sam <svu008@ucr.edu>
2019-02-09 14:43:34 -0800

    Debug Test 23 initialize color

--- grading/reflective_shader.cpp
+++ grading/reflective_shader.cpp
@@ -8,14 +8,15 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 {
     vec3 color;
 
-    if(recursion_depth >= world.recursion_depth_limit) {
+    if(recursion_depth > world.recursion_depth_limit) {
 	color = shader->Shade_Surface(ray,intersection_point,normal,recursion_depth);
     }
     else {
-	vec3 view_ray = ray.endpoint - intersection_point;
+	color = shader->Shade_Surface(ray,intersection_point,normal,recursion_depth);
+        vec3 view_ray = ray.endpoint - intersection_point;
         Ray reflected_ray(intersection_point, (2 * dot(view_ray,normal) * normal - view_ray).normalized());
 	color = (1 - reflectivity) * color + reflectivity * world.Cast_Ray(reflected_ray,recursion_depth+1);
     }
-    //if(debug_pixel) { std::cout << "reflective color: " << color << std::endl; }
+    if(debug_pixel) { std::cout << "(" << recursion_depth << "/" << world.recursion_depth_limit << ") reflective color: " << color << std::endl; }
     return color;
 }
