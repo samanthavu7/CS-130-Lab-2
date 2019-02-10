a275ecb8bfbf4efcc2c8675dfc7d7bdd9152fb4a
Sam <svu008@ucr.edu>
2019-02-09 21:04:32 -0800

    Debug Test 26

--- mesh.cpp
+++ mesh.cpp
@@ -1,4 +1,6 @@
 #include "mesh.h"
+#include "plane.h"
+#include "ray.h"
 #include <fstream>
 #include <string>
 #include <limits>
@@ -42,16 +44,27 @@ void Mesh::Read_Obj(const char* file)
 // Check for an intersection against the ray.  See the base class for details.
 Hit Mesh::Intersection(const Ray& ray, int part) const
 {
-    TODO;
-    return {};
+    double distance;
+
+    if(part >= 0) {
+        if(Intersect_Triangle(ray,part,distance)) { return {this,distance,part}; }
+    }
+    else {
+        int i = 0;
+        while(i < triangles.size() && !Intersect_Triangle(ray,i,distance)) { i++; }
+        if(i != triangles.size()) { return {this,distance,i}; }
+    } 
+    return {0,0,0};
 }
 
 // Compute the normal direction for the triangle with index part.
 vec3 Mesh::Normal(const vec3& point, int part) const
 {
     assert(part>=0);
-    TODO;
-    return vec3();
+    vec3 vertex1 = vertices.at(triangles[part][0]);
+    vec3 vertex2 = vertices.at(triangles[part][1]);
+    vec3 vertex3 = vertices.at(triangles[part][2]);
+    return cross(vertex1 - vertex2, vertex2 - vertex3).normalized();
 }
 
 // This is a helper routine whose purpose is to simplify the implementation
@@ -68,7 +81,30 @@ vec3 Mesh::Normal(const vec3& point, int part) const
 // two triangles.
 bool Mesh::Intersect_Triangle(const Ray& ray, int tri, double& dist) const
 {
-    TODO;
+    vec3 vertex1 = vertices.at(triangles[tri][0]);
+    vec3 vertex2 = vertices.at(triangles[tri][1]);
+    vec3 vertex3 = vertices.at(triangles[tri][2]);
+
+    Plane triangle_plane(vertex1, Normal(vertex1,tri));
+    Hit triangle_intersection = triangle_plane.Intersection(ray,tri);
+    if(!triangle_intersection.object) { return false; }
+    
+    vec3 p = ray.Point(triangle_intersection.dist);
+    vec3 v = vertex2 - vertex1;
+    vec3 w = vertex3 - vertex1;
+    vec3 y = p - vertex1;
+    vec3 u = ray.direction;
+
+    double beta = dot(cross(w,u), y) / dot(cross(w,u), v);
+    double gamma = dot(cross(u,v), y) / dot(cross(u,v), w);
+    double s = - dot(cross(v,w), y) / dot(cross(v,w), u);
+    double alpha = 1 - beta - gamma;
+
+    if(s > small_t && alpha >= -weight_tolerance && beta >= -weight_tolerance && gamma >= -weight_tolerance) {
+    	dist = triangle_intersection.dist;
+    	return true;
+    }
+    
     return false;
 }
 
